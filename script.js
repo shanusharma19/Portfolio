3330f4aad27fe4fbb3bb0ba6168e2ea45b0dfc69
shanu sharma <shanu04012003@gmail.com>
2022-11-20 15:47:36 +0530

    fixed about section

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
-for (var i = 0; i < navMenuAnchorTags.length; i++) {
-    navMenuAnchorTags[i].addEventListener('click', function (event) {
-        event.preventDefault();
-        var targetSectionID = this.textContent.trim().toLowerCase();
-        console.log(this.textContent);
-        var targetSection = document.getElementById(targetSectionID);
-        console.log(targetSection);
-        
-
-        var interval = setInterval(function () {
-            var targetSectionCoordinates = targetSection.getBoundingClientRect();
-            if ((targetSectionCoordinates.top <= 0) || (window.innerHeight + window.scrollY)>=document.body.scrollHeight) {
-                clearInterval(interval);
-                return;
-            }
-            window.scrollBy(0, 60); 
-        }, 20);
-    });
-}
-
 var progressBars = document.querySelectorAll(".skill-progress > div");
 
 function initialiseBar(bar) {
